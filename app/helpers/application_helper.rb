4fa3a670d783ae7c028a8061ae73b4aadf636e27
Barbaros Nicolae <barbarosnico@MacBook-Pro.local>
2017-03-06 12:53:08 +0200

    style(identation): translate tabs to spaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
-
 	def cp(path)
-    current_route = Rails.application.routes.recognize_path(path)
-    "active" if current_page?(path) or params[:controller] == current_route[:controller]
+		current_route = Rails.application.routes.recognize_path(path)
+		"active" if current_page?(path) or params[:controller] == current_route[:controller]
   end
 end
