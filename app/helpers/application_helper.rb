604bfa16b89cbd2dcc09f889647de9e4b5a83940
Barbaros Nicolae <barbarosnico@MacBook-Pro.local>
2017-03-06 12:40:39 +0200

    feat(header): check for current page location, add active class to the right nav link and style it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+	def cp(path)
+    current_route = Rails.application.routes.recognize_path(path)
+    "active" if current_page?(path) or params[:controller] == current_route[:controller]
+  end
 end
